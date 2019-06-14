4cfce454e7315f156e4c3dfffc03a863351fc20f
Xin <yangxin.nlp@bytedance.com>
2019-06-15 01:41:26 +0800

    update class 1

---
+++ 1.dev_env_basic_concepts/1_data_types.cpp
@@ -0,0 +1,70 @@
+/* 
+ * data types
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+// global variables can be viewed by all code blocks.
+int integer = 99;
+
+int main(){
+    // local variables can only be view within the area of curly braces.
+
+    // `int` means short range integers. In traditional programming 
+    // languages like Pascal, VB, etc., it typically use mem space 
+    // of 8 bits, which can refer to integers ranging from -2^7 to 
+    // 2^7-1. 
+    int integer = 100;
+
+    // `unsigned` means none negative integers. The difference between 
+    // `unsigned` and `int` is that it can only represents none negative
+    // numbers.
+    unsigned unsigned_integer = 200;
+
+    // `long` is larger `int`. Instead of using only 8- or 16-bit space, 
+    // `long` typically uses 32- or 64-bit space for an integer. It can 
+    // therefore represents larger range of integers.
+    long long_integer = 10000000;
+
+    long long very_long_integer;
+    unsigned long long very_long_positive_integer;
+    
+    // `float` represents decimal numbers. It only has precision of 6 decimal 
+    // places. I.e., it can't tell the difference between 0.0000001 and 0.0000002.
+    float floating_point_number = 0.000001;
+
+    // `double` has more precise decimal places than `float`.
+    // In practice, we typically use `double` instead of `float`.
+    double double_precision_floating_point_number = 0.0000000001;
+
+    // `char` represents a character. The character should be single quoted.
+    char character = 'a';
+
+    // `bool` has only 2 values: `true` or `false`.
+    bool bool_ = true;
+
+    // `void` means no type.
+    // void void_value = (void)integer;
+
+    // `arrays` represents a list of data, it can be `char`s, `int`s, `double`s, etc.
+    // the array length is defined in the bracket.
+    int integer_array[3] = {1, 2, 3};
+    double double_array[] = {1.0, 2.0, 3.0};
+    char str[] = "hello world";
+
+    // continously define many variables
+    int a, b, c = 0;
+    double e = 0.1, f, g;
+
+    // `cout` prints data to std output
+    cout << "hello world!" << endl;
+
+    // escape characters. E.g.:
+    cout << "hello\tworld\n\n";
+    
+    
+}
---
+++ 1.dev_env_basic_concepts/2_operaters.cpp
@@ -0,0 +1,56 @@
+/*
+ * operators
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath> // math operaters
+
+using namespace std;
+
+int main(){
+    // arithmatic operators
+    cout << "arithmatic operators:" << endl;
+    cout << "10 + 20 = "    << 10 + 20 << endl;
+    cout << "10.0 - 2.3 = " << 10.0 - 2.3 << endl;
+    cout << "2.3 + 2 = "    << 2.3 + 2 << endl;
+    cout << "4 * 5 = "      << 4 * 5 << endl;
+    cout << "21 / 5 = "     << 21 / 5 << endl;
+    cout << "21 % 5 = "     << 21 % 5 << endl;
+    cout << "21 / 5.0 = "   << 21 / 5.0 << endl;
+
+    int i = 1;
+    i++;
+    cout << "i = " << i << endl;
+    i--;
+    cout << "i = " << i << endl;
+
+    // relation operators
+    int a = 10, b = 21;
+    cout << "relation operators:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "a < b ? "      << (a < b) << endl;
+    cout << "a > b ? "      << (a > b) << endl;
+    cout << "a <= b ? "     << (a <= b) << endl;
+    cout << "a >= b ? "     << (a >= b) << endl;
+    cout << "a == b ? "     << (a == b) << endl;
+    cout << "a != b ? "     << (a == b) << endl;
+    
+    // 3-elem operator: () ? () : ()
+    a > b ? cout << "a is bigger" : cout << "b is bigger";
+
+    // logistic operators
+    bool sun_raises_from_east = true;
+    bool sun_raises_from_west = false;
+    cout << "&& (and) " << (sun_raises_from_east && sun_raises_from_west) << endl;
+    cout << "|| (or) " << (sun_raises_from_east || sun_raises_from_west) << endl;
+    cout << "! (not) " << (!sun_raises_from_east) << endl;
+
+    // bit operators
+    
+    // math operators
+    double d = 3.0;
+    cout << "sqrt(d) = " << sqrt(d) << endl;
+    cout << "exp(d) = " << exp(d) << endl;
+    // etc
+}
