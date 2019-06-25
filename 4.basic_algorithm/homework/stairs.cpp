cadd209b957f4cab68130a8e3b0484ba3a8048d8
Xin <yangxin.nlp@bytedance.com>
2019-06-25 17:51:38 +0800

    rename lint -> solution

--- 4.basic_algorithm/homework/stairs.cpp
+++ 4.basic_algorithm/homework/stairs.cpp
@@ -3,19 +3,19 @@
 #include <vector>
 
 using namespace std;
-typedef vector<int> lint;
+typedef vector<int> solution;
 
-vector<lint> stairs(int n){
+vector<solution> stairs(int n){
     if (n == 1){
-        lint steps;
+        solution steps;
         steps.push_back(1);
-        vector<lint> result;
+        vector<solution> result;
         result.push_back(steps);
         return result;
     } else if (n == 2) {
-        vector<lint> result;
-        lint step1;
-        lint step2;
+        vector<solution> result;
+        solution step1;
+        solution step2;
         step1.assign(2, 1);
         step2.push_back(2);
         result.push_back(step1);
@@ -23,50 +23,50 @@ vector<lint> stairs(int n){
         return result;
     }
 
-    vector<lint> one_res = stairs(n-1);
-    vector<lint> two_res = stairs(n-2);
+    vector<solution> one_res = stairs(n-1);
+    vector<solution> two_res = stairs(n-2);
     
-    for (lint & step : one_res){
+    for (solution & step : one_res){
         step.push_back(1);
     }
-    for (lint & step : two_res){
+    for (solution & step : two_res){
         step.push_back(2);
     }
     
-    vector<lint> &result = one_res;
+    vector<solution> &result = one_res;
     result.insert(result.end(), two_res.begin(), two_res.end());
 
     return result;
 }
 
-vector<lint> stairs2(int n){
-    vector<vector<lint> > steps;
+vector<solution> stairs2(int n){
+    vector<vector<solution> > steps;
     
-    lint step1;
+    solution step1;
     step1.push_back(1);
-    vector<lint> steps1;
+    vector<solution> steps1;
     steps1.push_back(step1);
     steps.push_back(steps1);
 
-    lint step2;
-    lint step3;
+    solution step2;
+    solution step3;
     step2.assign(2, 1);
     step3.push_back(2);
-    vector<lint> steps2;
+    vector<solution> steps2;
     steps2.push_back(step2);
     steps2.push_back(step3);
     steps.push_back(steps2);
 
     for (int i = 2; i < n; i++){
-        vector<lint> one_res(steps[i-1]);
-        vector<lint> two_res(steps[i-2]);
-        for (lint & step : one_res){
+        vector<solution> one_res(steps[i-1]);
+        vector<solution> two_res(steps[i-2]);
+        for (solution & step : one_res){
             step.push_back(1);
         }
-        for (lint & step : two_res){
+        for (solution & step : two_res){
             step.push_back(2);
         }
-        vector<lint> & step = one_res;
+        vector<solution> & step = one_res;
         step.insert(step.end(), two_res.begin(), two_res.end());
         steps.push_back(step);
     }
@@ -78,10 +78,10 @@ int main(){
     int n = 5;
     // cin >> n;
     
-    vector<lint> result = stairs2(n);
+    vector<solution> result = stairs2(n);
     
     cout << "total steps = " << result.size() << endl;
-    for (lint & step : result){
+    for (solution & step : result){
         for (int s : step){
             cout << s << ' ';
         }
